2e879135a01d22e81f392b48204519b1e1a7a9b2
workuanzeno <127034912+workuanzeno@users.noreply.github.com>
2023-04-28 03:05:06 -0700

    Add files via upload

---
+++ j.js
@@ -0,0 +1,21 @@
+
+
+let num1= 50
+let num2 = 51
+sum = num1 + num2
+
+if(sum < -100){
+    console.log("101 is greater than 100")
+        
+} else if (sum + " is a negative number") 
+{console.log(sum + "97 is greater than 0")
+    
+    
+}
+
+
+
+
+
+
+
